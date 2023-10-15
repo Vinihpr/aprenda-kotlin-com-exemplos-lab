8d4993ec6f0e198a2f0fce7c22ae85db93d58a01
Vinicius <143199128+Vinihpr@users.noreply.github.com>
2023-10-15 18:44:40 -0300

    versão 2

--- desafio.kt
+++ desafio.kt
@@ -1,21 +1,44 @@
-// [Template no Kotlin Playground](https://pl.kotl.in/WcteahpyN)
-
 enum class Nivel { BASICO, INTERMEDIARIO, DIFICIL }
 
-class Usuario
+data class Usuario(val nome: String)
 
-data class ConteudoEducacional(var nome: String, val duracao: Int = 60)
+data class ConteudoEducacional(val nome: String, val duracao: Int = 60)
 
-data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) {
-
+class Formacao(val nome: String, val nivel: Nivel, val conteudos: List<ConteudoEducacional>) {
     val inscritos = mutableListOf<Usuario>()
-    
+
     fun matricular(usuario: Usuario) {
-        TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
+        inscritos.add(usuario)
+        println("${usuario.nome} foi matriculado na formação: $nome")
+    }
+
+    fun listarInscritos() {
+        if (inscritos.isEmpty()) {
+            println("Nenhum usuário matriculado na formação: $nome")
+        } else {
+            println("Usuários matriculados na formação: $nome")
+            inscritos.forEach { println(it.nome) }
+        }
     }
 }
 
 fun main() {
-    TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
-    TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
+    val usuario1 = Usuario("Vinicius")
+    val usuario2 = Usuario("Maria")
+    val usuario3 = Usuario("José")
+
+    val conteudo1 = ConteudoEducacional("Introdução à Kotlin", 120)
+    val conteudo2 = ConteudoEducacional("Estruturas de Dados", 180)
+
+    val formacaoBasica = Formacao("Curso Básico de Kotlin", Nivel.BASICO, listOf(conteudo1))
+    val formacaoAvancada = Formacao("Curso Avançado de Kotlin", Nivel.DIFICIL, listOf(conteudo1, conteudo2))
+
+    formacaoBasica.matricular(usuario1)
+    formacaoBasica.matricular(usuario2)
+
+    formacaoAvancada.matricular(usuario2)
+    formacaoAvancada.matricular(usuario3)
+
+    formacaoBasica.listarInscritos()
+    formacaoAvancada.listarInscritos()
 }
